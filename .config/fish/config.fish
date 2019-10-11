c407bf8eb84eb168661ac04344f633fa7442f5b6
Tiago Sousa <tjisousa@gmail.com>
2019-10-11 22:15:59 +0200

    added .config directory.

---
+++ .config/fish/config.fish
@@ -0,0 +1,42 @@
+# Set the emoji width for iTerm
+set -g fish_emoji_width 2
+
+# Hide the fish greeting
+set fish_greeting ""
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+# Install Starship
+eval (starship init fish)
+
+
+# Miniconda
+set PATH ~/miniconda3/bin $PATH
+
+eval (starship init fish)
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/tiagosousa/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
